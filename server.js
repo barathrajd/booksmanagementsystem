0cf1a388df094c8b37efc89e9aa9193ce8d7e5a6
barathrajd <barathdeva757@gmail.com>
2021-04-23 09:45:39 +0530

    Sever js

--- server.js
+++ server.js
@@ -18,38 +18,11 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-if ((process.env.NODE_ENV || '').trim() === 'production') {
-  app.use((req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      res.redirect(302, 'https://' + req.hostname + req.originalUrl);
-    } else {
-      next();
-    }
-  });
-  app.use(express.static('views/build'));
-}
-
-app.use((req, res, next) => {
-  if ((process.env.NODE_ENV || '').trim() !== 'production') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-auth-token');
-  }
-  next();
+app.get('/', (req, res) => {
+  res.send('Welcome to BMS API');
 });
 
 app.use('/api', routes);
-
-app.use('/*', (req, res) => {
-  if ((process.env.NODE_ENV || '').trim() == 'production') {
-    res.sendFile(__dirname + '/views/build/index.html');
-  } else {
-    res.send('Welcome to Book Management System API');
-  }
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(
