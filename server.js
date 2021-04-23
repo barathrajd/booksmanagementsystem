89cd0b3e8f90982ec7d0be7cc7d09301a1d9e8ef
barathrajd <barathdeva757@gmail.com>
2021-04-23 09:03:42 +0530

    Sever js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const routes = require('./routes/routes');
 const { notFound, errorHandler } = require('./middleware/error');
@@ -9,12 +10,27 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Welcome to Book Management System API');
 });
 
 app.use('/', routes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/views/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'views', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 
