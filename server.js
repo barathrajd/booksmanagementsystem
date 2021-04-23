f870f04a994fc86391db1a146213d83b3631786e
barathrajd <barathdeva757@gmail.com>
2021-04-23 10:20:19 +0530

    Deploy to Herokku testing 2

--- server.js
+++ server.js
@@ -15,12 +15,37 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+app.use((req, res, next) => {
+  if ((process.env.NODE_ENV || '').trim() !== 'production') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-auth-token');
+  }
+  next();
+});
+
+app.use('/api', routes);
+
+if ((process.env.NODE_ENV || '').trim() === 'production') {
+  app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      res.redirect(302, 'https://' + req.hostname + req.originalUrl);
+    } else {
+      next();
+    }
+  });
+  app.use(express.static('views/build'));
+}
+
 // Server Static files
-if ((process.env.NODE_ENV = 'production')) {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('views/build'));
 
-  app.get('*', (req, res) => {
+  app.use('/*', (req, res) => {
     res.send(path.resolve(__dirname, 'views', 'build', 'index.html'));
   });
 } else {
@@ -28,8 +53,6 @@ if ((process.env.NODE_ENV = 'production')) {
     res.send('Welcome to BMS API');
   });
 }
-
-app.use('/api', routes);
 
 app.use(notFound);
 
