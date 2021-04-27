2656f42e0fd47136dc42029068616d214fcdf24c
barathrajd <barathdeva757@gmail.com>
2021-04-27 13:24:09 +0530

    Admin GetUser By Id and Update User By Id Backend

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -26,4 +26,44 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getUsers, deleteUser };
+// @desc Get User By Id User
+// @route Get /api/admin/users/:id
+// @access Private/Admin
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User Not Found');
+  }
+});
+
+// @desc Update Any User
+// @route PUT /api/admin/users/:id
+// @access Private/Admin
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User Not found');
+  }
+});
+
+module.exports = { getUsers, deleteUser, getUserById, updateUser };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { getUsers, deleteUser } = require('../controllers/adminController');
+const {
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} = require('../controllers/adminController');
 const { auth, admin } = require('../middleware/authentication');
 const router = express.Router();
 
@@ -8,5 +13,11 @@ router.get('/users', auth, admin, getUsers);
 
 // Delete a User
 router.delete('/user/:id', auth, admin, deleteUser);
+
+// GET User By Id
+router.get('/users/:id', auth, admin, getUserById);
+
+// Update User
+router.put('/users/:id', auth, admin, updateUser);
 
 module.exports = router;
